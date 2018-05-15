bf7249e4b949e244c9d0014a6604567737012d61
eduardo.abi <eduardo.abi@despegar.com>
2018-05-15 16:04:06 -0300

    updating schema

--- conf/evolutions.default/1.sql
+++ conf/evolutions.default/1.sql
@@ -1,103 +1,252 @@
-# --- !Ups
+-- MySQL dump 10.13  Distrib 5.7.22, for Linux (x86_64)
+--
+-- Host: localhost    Database: emei
+-- ------------------------------------------------------
+-- Server version	5.7.22-0ubuntu0.16.04.1
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alumnos`
+--
 use emei;
-create table IF NOT EXISTS `nivel` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `nivel` varchar(255) not null
-);
+DROP TABLE IF EXISTS `alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alumnos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `legajo` varchar(255) NOT NULL,
+  `nombres` varchar(255) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
+  `nivel_grado` int(11) DEFAULT NULL,
+  `familia` int(11) NOT NULL,
+  `activo` varchar(45) DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `alumnos_ibfk_1_idx` (`familia`),
+  KEY `alumnos_ibfk_2` (`nivel_grado`),
+  CONSTRAINT `alumnos_ibfk_1` FOREIGN KEY (`familia`) REFERENCES `familia` (`id`),
+  CONSTRAINT `alumnos_ibfk_2` FOREIGN KEY (`nivel_grado`) REFERENCES `nivel_grado` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table IF NOT EXISTS `familia` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `descripcion` varchar(255) not null,
-  `observaciones` varchar(255) not null
+--
+-- Table structure for table `anio`
+--
 
-);
+DROP TABLE IF EXISTS `anio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `anio` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `anio` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table IF NOT EXISTS `tipo_contacto` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `descripcion` varchar(255) not null
-);
+--
+-- Table structure for table `contacto`
+--
 
-create table IF NOT EXISTS `contacto` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `descripcion` varchar(255) not null,
-  `tipo_contacto` integer  not null,
-  FOREIGN KEY (tipo_contacto) REFERENCES tipo_contacto (ID)
+DROP TABLE IF EXISTS `contacto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `contacto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) NOT NULL,
+  `familia_id` int(11) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-);
+--
+-- Table structure for table `cuota`
+--
 
-create table IF NOT EXISTS `grado` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `grado` varchar(255) not null
-  );
+DROP TABLE IF EXISTS `cuota`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cuota` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `anio` int(11) NOT NULL,
+  `mes` int(11) NOT NULL,
+  `cuota_base` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `anio` (`anio`),
+  KEY `mes` (`mes`),
+  KEY `cuota_base` (`cuota_base`),
+  CONSTRAINT `cuota_ibfk_1` FOREIGN KEY (`anio`) REFERENCES `anio` (`id`),
+  CONSTRAINT `cuota_ibfk_2` FOREIGN KEY (`mes`) REFERENCES `mes` (`id`),
+  CONSTRAINT `cuota_ibfk_3` FOREIGN KEY (`cuota_base`) REFERENCES `cuota_base` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `cuota_base`
+--
 
-create table IF NOT EXISTS `nivel_grado` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `nivel` integer not null,
-  `grado` integer not null,
-  FOREIGN KEY (nivel) REFERENCES nivel (ID),
-  FOREIGN KEY (grado) REFERENCES grado (ID)
-);
+DROP TABLE IF EXISTS `cuota_base`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cuota_base` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `valor` double NOT NULL,
+  `anio` int(11) NOT NULL,
+  `mes` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `anio` (`anio`),
+  KEY `mes` (`mes`),
+  CONSTRAINT `cuota_base_ibfk_1` FOREIGN KEY (`anio`) REFERENCES `anio` (`id`),
+  CONSTRAINT `cuota_base_ibfk_2` FOREIGN KEY (`mes`) REFERENCES `mes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `familia`
+--
 
-create table IF NOT EXISTS `alumnos` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `legajo` varchar(255) not null,
-  `nombres` varchar(255) not null,
-  `apellidos` varchar(255) not null,
-  `nivel_grado` integer not null,
-  `familia` integer  not null,
-    FOREIGN KEY (familia) REFERENCES familia (ID),
-    FOREIGN KEY (nivel_grado) REFERENCES nivel_grado (ID)
-);
+DROP TABLE IF EXISTS `familia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `familia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) NOT NULL,
+  `observaciones` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `grado`
+--
 
-create table IF NOT EXISTS `anio` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `anio` integer not null
-);
+DROP TABLE IF EXISTS `grado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `grado` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table IF NOT EXISTS `mes` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `mes` integer not null
-);
+--
+-- Table structure for table `mes`
+--
 
+DROP TABLE IF EXISTS `mes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mes` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table IF NOT EXISTS `tipo_pago` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `descripcion` varchar(255) not null
-);
+--
+-- Table structure for table `nivel`
+--
 
-create table IF NOT EXISTS `cuota_base` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `valor` double not null,
-  `anio` integer not null,
-  `mes` integer  not null,
-  FOREIGN KEY (anio) REFERENCES anio (ID),
-  FOREIGN KEY (mes) REFERENCES mes (ID)
-);
+DROP TABLE IF EXISTS `nivel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nivel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nivel` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table IF NOT EXISTS `cuota` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `anio` integer not null,
-  `mes` integer  not null,
-  `cuota_base` integer not null,
-  FOREIGN KEY (anio) REFERENCES anio (ID),
-  FOREIGN KEY (mes) REFERENCES mes (ID),
-  FOREIGN KEY (cuota_base) REFERENCES cuota_base (ID)
+--
+-- Table structure for table `nivel_grado`
+--
 
+DROP TABLE IF EXISTS `nivel_grado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `nivel_grado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nivel` int(11) NOT NULL,
+  `grado` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `nivel` (`nivel`),
+  KEY `grado` (`grado`),
+  CONSTRAINT `nivel_grado_ibfk_1` FOREIGN KEY (`nivel`) REFERENCES `nivel` (`id`),
+  CONSTRAINT `nivel_grado_ibfk_2` FOREIGN KEY (`grado`) REFERENCES `grado` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-);
+--
+-- Table structure for table `pago`
+--
 
-create  table IF NOT EXISTS `pago` (
-  `id` integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `recibo` varchar(255) not null,
-  `descuento_aplicado` double null ,
-  `tipo_pago` integer not null,
-  `familia` integer not null,
-  `cuota` integer not null,
-   FOREIGN KEY (tipo_pago) REFERENCES tipo_pago (ID),
-   FOREIGN KEY (familia) REFERENCES familia (ID),
-   FOREIGN KEY (cuota) REFERENCES cuota (ID)
-);
+DROP TABLE IF EXISTS `pago`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pago` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recibo` varchar(255) NOT NULL,
+  `descuento_aplicado` double DEFAULT NULL,
+  `tipo_pago` int(11) NOT NULL,
+  `familia` int(11) NOT NULL,
+  `cuota` int(11) NOT NULL,
+  `interes` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tipo_pago` (`tipo_pago`),
+  KEY `familia` (`familia`),
+  KEY `cuota` (`cuota`),
+  CONSTRAINT `pago_ibfk_1` FOREIGN KEY (`tipo_pago`) REFERENCES `tipo_pago` (`id`),
+  CONSTRAINT `pago_ibfk_2` FOREIGN KEY (`familia`) REFERENCES `familia` (`id`),
+  CONSTRAINT `pago_ibfk_3` FOREIGN KEY (`cuota`) REFERENCES `cuota` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tipo_contacto`
+--
+
+DROP TABLE IF EXISTS `tipo_contacto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipo_contacto` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `tipo_pago`
+--
+
+DROP TABLE IF EXISTS `tipo_pago`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipo_pago` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-15 15:56:57
